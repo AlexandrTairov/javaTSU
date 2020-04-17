701e6e7e7da95ae830d5d69ff5f9b71475213477
AlexandrTairov <sanek.tai0@gmail.com>
2020-04-17 23:16:09 +0400

    Lab4

---
+++ src/lab4/part8/CalculatorStandartEdition.java
@@ -0,0 +1,12 @@
+package lab4.part8;
+
+public class CalculatorStandartEdition {
+
+    public double calculateF1(double x) {
+        return Math.cos(x);
+    }
+
+    public double calculateF2(double x) {
+        return Math.pow(x, 1.0d / 3.0d);
+    }
+}
